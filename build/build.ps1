21d3d80cdd0019785d3eb11fa634ab066240ac97
Tony Lea <tonylea@users.noreply.github.com>
2023-06-06 11:31:41 +0100

    ci: add unit tests for linux

---
+++ build/build.ps1
@@ -0,0 +1,86 @@
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory)]
+    [System.IO.FileInfo]
+    $DependencyConfigPath,
+
+    [Parameter()]
+    [Switch]
+    $ResolveDependency,
+
+    [Parameter()]
+    [Switch]
+    $ImportProjectModule,
+
+    [Parameter(Mandatory)]
+    [ValidateSet("UnitTests")]
+    [System.String]
+    $Task,
+
+    [Parameter()]
+    [System.String]
+    $OperatingSystem
+)
+
+if ($ResolveDependency.IsPresent) {
+    Write-Host "`nSTARTED TASK: Install dependencies" -ForegroundColor Blue
+
+    if (-not ((Get-PSRepository PSGallery).InstallationPolicy -like "Trusted")) {
+        Write-Host "`n  Set PSGallery as trusted source"
+        Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+    }
+
+    Get-PackageProvider -Name "NuGet" -ForceBootstrap | Out-Null
+
+    # Install PSDepend module if it is not already installed
+    if (-not (Get-Module -Name "PSDepend" -ListAvailable)) {
+        Write-Host "`n  Installing PSDepend"
+        Install-Module -Name "PSDepend" -Scope "CurrentUser" -Force
+    }
+    else {
+        Write-Host "`n  PSDepend already installed"
+    }
+
+    # Install build dependencies
+    $DependencyConfigPath = Join-Path -Path $PSScriptRoot -ChildPath "dependancies.psd1"
+    Write-Host "`n  Resolving module dependencies from $DependencyConfigPath"
+    Import-Module -Name "PSDepend"
+    $InvokePSDependArgs = @{
+        Path    = $DependencyConfigPath
+        Import  = $true
+        Confirm = $false
+        Install = $true
+    }
+    Invoke-PSDepend @InvokePSDependArgs
+}
+else {
+    Write-Host "`nSKIPPED TASK: Install dependencies" -ForegroundColor Blue
+}
+
+# Init BuildHelpers
+Write-Host "`nSTARTED TASK: Set environmental variables" -ForegroundColor Blue
+Set-BuildEnvironment -Force
+
+# Load Ansi Colours Write-Host
+Write-Host "`nSTARTED TASK: Overload Write-Host with Ansi colours" -ForegroundColor Blue
+$AnsiWriteHostOverloadPath = Join-Path -Path $ENV:BHProjectPath -ChildPath "utils" -AdditionalChildPath "AnsiWriteHostOverload.ps1"
+. $AnsiWriteHostOverloadPath
+
+if ($ImportProjectModule.IsPresent) {
+    Write-Host "`nSTARTED TASK: Importing project module into scope" -ForegroundColor Blue
+    Import-Module -Name $ENV:BHPSModuleManifest -Force
+}
+
+# Execute psake tasks
+Write-Host -AnsiColors "`nSTARTED TASK: $Task" -ForegroundColor Green
+
+$InvokePsakeArgs = @{
+    buildFile = Join-Path -Path $ENV:BHProjectPath -ChildPath "build" -AdditionalChildPath "build.psake.ps1"
+    nologo    = $true
+    taskList  = $Task
+}
+Invoke-Psake @InvokePsakeArgs
+
+# Write-Host -AnsiColors "`nFINISHED TASKS" -ForegroundColor Green
+
+# exit ( [int](-not $psake.build_success) )
---
+++ build/dependancies.psd1
@@ -0,0 +1,9 @@
+@{
+    PSDependOptions = @{
+        Target     = "CurrentUser"
+        Repository = "PSGallery"
+    }
+    BuildHelpers    = "2.0.16"
+    Pester          = "5.4.1"
+    psake           = "4.9.0"
+}
