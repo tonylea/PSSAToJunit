e665f92ec1b7da80bc6088d17d3e55fa6b093df9
Tony Lea <tonylea@users.noreply.github.com>
2023-06-08 11:53:03 +0100

    fix: correct import filenames to lower case

--- src/PSSAToJunit.psm1
+++ src/PSSAToJunit.psm1
@@ -1,13 +1,13 @@
 #Get public and private function definition files.
-$ClassesPath = Join-Path -Path $PSScriptRoot -ChildPath "Classes" -AdditionalChildPath "*.ps1"
+$ClassesPath = Join-Path -Path $PSScriptRoot -ChildPath "classes" -AdditionalChildPath "*.ps1"
 $Classes = @( Get-ChildItem -Path $ClassesPath -ErrorAction SilentlyContinue )
 
-$PrivatePath = Join-Path -Path $PSScriptRoot -ChildPath "Private" -AdditionalChildPath "*.ps1"
+$PrivatePath = Join-Path -Path $PSScriptRoot -ChildPath "private" -AdditionalChildPath "*.ps1"
 Write-Host "Private Path: $PrivatePath" -ForegroundColor Red
 $Private = @( Get-ChildItem -Path $PrivatePath -ErrorAction SilentlyContinue )
 Write-Host "Found $($Private.Count) private functions"
 
-$PublicPath = Join-Path -Path $PSScriptRoot -ChildPath "Public" -AdditionalChildPath "*.ps1"
+$PublicPath = Join-Path -Path $PSScriptRoot -ChildPath "public" -AdditionalChildPath "*.ps1"
 Write-Host "Public Path: $PublicPath" -ForegroundColor Red
 $Public = @( Get-ChildItem -Path $PublicPath -ErrorAction SilentlyContinue )
 Write-Host "Found $($Public.Count) public functions"
